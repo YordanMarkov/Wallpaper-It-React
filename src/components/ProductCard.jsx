9f184279a3a9405d1be474f565f6826c96ac58b9
YordanMarkov <yordanmarkov2004@gmail.com>
2023-04-16 20:51:14 +0300

    Using the given API. Added rating and sort Newest - Oldest. Христос Воскресе! 16.04.2023

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,32 @@
 import './ProductCard.css';
+import { FaStar, FaRegStar } from 'react-icons/fa';
 
-const ProductCard = ({ url, title, price }) => {
+const ProductCard = ({ url, title, price, rating }) => {
+  const starRating = [];
+  for (let i = 0; i < 5; i++) {
+    if (i < rating) {
+      starRating.push(<FaStar key={i} />);
+    } else {
+      starRating.push(<FaRegStar key={i} className="empty-star" />);
+    }
+  }
 
   return (
     <article className="p-4 flex flex-col justify-center gap-y-4">
-      <img src={url} alt="Product Logo" className="cursor-pointer" />
-      <p className="cursor-pointer hover:underline text-base">{title}</p>
-      <div className="w-full flex justify-between items-center">
-        <p className="text-sm">£{price}</p>
-        <button className="underline cursor-pointer text-sm">
-          Order example
-        </button>
-      </div>
-      <p className="text-base uppercase">Per roll</p>
-    </article>
+       <img src={url} alt="Product Logo" className="cursor-pointer" />
+       <p className="cursor-pointer hover:underline text-base">{title}</p>
+       <div className='stars'>
+          <span className='star-rating'>{starRating}</span>
+          <span className='rating'>{Number(rating).toFixed(1)}</span>
+       </div>
+       <div className="w-full flex justify-between items-center">
+         <p className="text-sm">£{price}</p>
+         <button className="underline cursor-pointer text-sm">
+           Order example
+         </button>
+       </div>
+       <p className="text-base uppercase">Per roll</p>
+     </article>
   );
 };
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -12,7 +12,7 @@ const ProductList = () => {
     const fetchData = async () => {
       try {
         const result = await fetch(
-          "https://jsonplaceholder.typicode.com/photos?_start=0&_limit=20"
+          "https://tues-store.dev.uxperience.eu/wp-json/wc/v3/products?consumer_key=ck_b9f04fe6e6c9194e1e31580b279baea4d26bc1d6&consumer_secret=cs_0f0a0925f29d0fbd6c55c71f630b50a430bcc039"
         );
 
         if (!result.ok) {
@@ -20,7 +20,23 @@ const ProductList = () => {
         }
 
         const data = await result.json();
-        setProductList(data);
+        // Manually set rating for products with id 41 & 40 just to show how it works
+        const updatedData = data.map(product => {
+          if (product.id === 41) {
+            return {
+              ...product,
+              average_rating: 4.8
+            };
+          } else if (product.id === 40) {
+            return {
+              ...product,
+              average_rating: 2.9
+            };
+          } else {
+            return product;
+          }
+        });
+        setProductList(updatedData);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -34,11 +50,16 @@ const ProductList = () => {
   const getPrice = useMemo(() => {
     const prices = {};
     productList.forEach(product => {
-      const price = (Math.floor(Math.random() * 4800) + 1100) / 100;
+      const price = product.price;
       prices[product.id] = price;
     });
     return prices;
   }, [productList]);
+
+  const getDateCreated = (dateString) => {
+    const date = new Date(dateString);
+    return date;
+  } 
 
   const sortedProductList = useMemo(() => {
     const sortedList = [...productList];
@@ -46,6 +67,8 @@ const ProductList = () => {
       sortedList.sort((a, b) => getPrice[a.id] - getPrice[b.id]);
     } else if (sortOrder === 'desc') {
       sortedList.sort((a, b) => getPrice[b.id] - getPrice[a.id]);
+    } else if (sortOrder === 'newest') {
+      sortedList.sort((a, b) => getDateCreated(a.date_created) - getDateCreated(b.date_created));
     }
     return sortedList;
   }, [productList, getPrice, sortOrder]);
@@ -80,7 +103,7 @@ const ProductList = () => {
             <option value="">Sort</option>
             <option value="desc">Price: High - Low</option>
             <option value="asc">Price: Low - High</option>
-            <option value="">Newest - Oldest</option>
+            <option value="newest">Newest - Oldest</option>
           </select>
         </div>
       </div>
@@ -88,9 +111,10 @@ const ProductList = () => {
         {sortedProductList.map((product) => (
           <ProductCard
             key={product.id}
-            title={product.title}
-            url={product.url}
-            price={getPrice[product.id]}
+            title={product.name}
+            url={product.images[0].src}
+            price={product.price}
+            rating={product.average_rating}
           />
         ))}
       </div>
